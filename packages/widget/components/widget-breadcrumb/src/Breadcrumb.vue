783e43e0350d2efcd15799062d622915eb5c4493
yaying.li <yaying.li@sky-data.cn>
2019-04-19 18:10:33 +0800

    feat(widget): add breadcrumb widget
    1. add breadcrumb widget

---
+++ packages/widget/components/widget-breadcrumb/src/Breadcrumb.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="bread-crumb-container">
+        <el-breadcrumb
+            :separator="seperator"
+            :separator-class="seperatorClass"
+            v-if="breadcrumbs.length"
+        >
+            <el-breadcrumb-item
+                v-for="(crumb, index) in breadcrumbs"
+                :to="crumb"
+                :key="index"
+            >
+                {{ crumb.meta.breadcrumb }}
+            </el-breadcrumb-item>
+        </el-breadcrumb>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Breadcrumb',
+    data() {
+        return {
+            breadcrumbs: []
+        };
+    },
+    props: {
+        seperator: {
+            type: String,
+            default: '/'
+        },
+        seperatorClass: {
+            type: String,
+            default: ''
+        }
+    },
+    watch: {
+        $route: {
+            handler(nVal) {
+                this.breadcrumbs = [];
+                nVal.matched.forEach(route => {
+                    if (route.meta && route.meta.breadcrumb) {
+                        this.breadcrumbs.push(route);
+                    }
+                });
+            },
+            immediate: true
+        }
+    }
+};
+</script>

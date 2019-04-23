712674e7aa2c83a3372c3a0b3c5e20a5e6bb6e5c
xiafei.wu <xiaofei.wu@iluvatar.ai>
2019-04-23 10:51:20 +0800

    feat(feb-alive): feb-alive

---
+++ examples/src/views/cache/b.vue
@@ -0,0 +1,74 @@
+<template>
+    <div>
+        <h1>我是B页面</h1>
+        <p>count: {{ count }}</p>
+        <p class="page-action">
+            <button
+                class="page-action--item"
+                @click="count += 1"
+            >
+                增加
+            </button>
+        </p>
+        <p class="page-link">
+            <router-link to="/Cache/a/123456">
+                前往A页面
+            </router-link>
+        </p>
+        <p class="page-link">
+            <button @click="toReturn">
+                返回
+            </button>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            count: 0,
+            fullPath: ''
+        };
+    },
+    beforeRouteEnter(to, from, next) {
+        console.log(to, from, 'b');
+        next(vm => vm.fullPath = from.fullPath);   
+    },
+    methods: {
+        toReturn() {
+            this.$router.push(this.fullPath);
+        }
+    }
+};
+</script>
+
+<style>
+h1 {
+  text-align: center;
+}
+p {
+  text-align: center;
+}
+.page-action {
+  /* position: fixed;
+  bottom: 100px; */
+  margin-top: 30px;
+  width: 100%;
+  text-align: center;
+}
+.page-action--item {
+  display: inline-block;
+  width: 120px;
+  height: 30px;
+  color: #fff;
+  line-height: 30px;
+  background: #3f86ff;
+  border: none;
+  border-radius: 5px;
+  outline: none;
+}
+.page-link {
+  margin-top: 50px;
+}
+</style>

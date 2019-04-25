f540ccbd96fdd75466562c4c4cee0da723eb77ca
xiafei.wu <xiaofei.wu@iluvatar.ai>
2019-04-25 18:01:46 +0800

    feat(add transfer): transfer

--- packages/ui/components/transfer/src/Transfer.vue
+++ packages/ui/components/transfer/src/Transfer.vue
@@ -1,44 +1,163 @@
 <template>
     <div class="sdxu-transfer">
-        <el-transfer
-            v-model="value"
-            :data="data"
-        />
-        <div>
-            fasf
+        <div class="tree">
+            <el-input
+                size="small"
+                placeholder="请输入"
+                suffix-icon="el-icon-search"
+                v-model="filterText"
+            />
+            <el-tree
+                :data="data"
+                show-checkbox
+                node-key="id"
+                ref="tree"
+                highlight-current
+                :filter-node-method="filterNode"
+                check-on-click-node
+                :default-checked-keys="defaultKeys"
+            />
+            <div class="moveAll">
+                <span @click="moveAllTag">
+                    移动全部
+                </span>
+            </div>
+        </div>
+        <div class="moveIcon">
+            <span @click="movetag">
+                =
+            </span>
+        </div>
+        <div class="tag">
+            <el-input
+                size="small"
+                placeholder="请输入"
+                suffix-icon="el-icon-search"
+                v-model="filterTag"
+            />
+            <div class="tagItem">
+                <el-tag
+                    v-for="tag in tags"
+                    :key="tag.id"
+                    closable
+                    @close="handleClose(tag)"
+                    :class="tag.is_group ? 'group' : 'user'"
+                >
+                    {{ tag.name }}
+                </el-tag>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-import {Transfer} from 'element-ui';
+import {Tree,Input, Tag} from 'element-ui';
 export default {
     name: 'SdxuTransfer',
     data() {
-        const generateData = _ => {
-            const data = [];
-            for (let i = 1; i <= 15; i++) {
-                data.push({
-                    key: i,
-                    label: `备选项 ${ i }`,
-                    disabled: i % 4 === 0
-                });
-            }
-            return data;
-        };
         return {
-            data: generateData(),
-            value: [1, 4]
+            data: [{
+                id: 1,
+                label: '一级 1',
+                children: [{
+                    id: 4,
+                    label: '二级 1-1',
+                }]
+            }, {
+                id: 2,
+                label: '一级 2',
+                children: [{
+                    id: 5,
+                    label: '二级 2-1'
+                }, {
+                    id: 6,
+                    label: '二级 2-2'
+                }]
+            }, {
+                id: 3,
+                label: '一级 3',
+                children: [{
+                    id: 7,
+                    label: '二级 3-1'
+                }, {
+                    id: 8,
+                    label: '二级 3-2'
+                }]
+            }],
+            filterText: '',
+            filterTag: '',
+            tags: [],
+            currentData: [],
+            defaultKeys: [],
+            saveTags: ''
         };
     },
     components: {
-        [Transfer.name]: Transfer
+        [Tree.name]: Tree,
+        [Input.name]: Input,
+        [Tag.name]: Tag
     },
     methods: {
-        // handleChange(value, direction, movedKeys) {
-        //     console.log(value, direction, movedKeys);
-        // }
-    }
+        filterNode(value, data) {
+            if (!value) return true;
+            return data.label.indexOf(value) !== -1;
+        },
+        movetag() {
+            let [checkedNodes ,childrenKeys, moveNodes, tags]= [this.$refs.tree.getCheckedNodes(),[], [], []];  
+            for(let i =0; i< checkedNodes.length; i ++) {
+                if(checkedNodes[i].children ) {
+                    for (let j =0; j< checkedNodes[i].children.length; j ++) {
+                        childrenKeys.push(checkedNodes[i].children[j].id);
+                    }
+                }
+            }
+            moveNodes = checkedNodes.filter(v => !childrenKeys.includes(v.id));
+            for (let i =0; i < moveNodes.length ; i++ ) {
+                tags.push(
+                    {
+                        name: moveNodes[i].label,
+                        id: moveNodes[i].id,
+                        is_group: moveNodes[i].children ? true : false
+                    }
+                );
+            }
+            this.tags = tags;
+        },
+        handleClose(tag) {
+            this.tags.splice(this.tags.indexOf(tag), 1);
+            this.saveTags = JSON.stringify(this.tags);
+        },
+        moveAllTag() {
+            let [tags, keys] = [[], []];
+            for (let i =0; i < this.data.length; i ++) {
+                tags.push(
+                    {
+                        name: this.data[i].label,
+                        id: this.data[i].id,
+                        is_group:  true 
+                    }
+                );
+                keys.push(this.data[i].id);
+                if (this.data[i].children) {
+                    for(let i =0; i < this.data[i].children.length ; i++) {
+                        keys.push(this.data[i].children[i].id);
+                    }
+                }
+            }
+            this.defaultKeys = keys;
+            this.tags = tags;
+            this.saveTags = JSON.stringify(tags);
+        }
+    },
+    watch: {
+        filterText(val) {
+            this.$refs.tree.filter(val);
+        },
+        filterTag(val) {
+            let tag = JSON.parse(this.saveTags).filter(item => item.name.includes(val));
+            this.tags = tag;
+        }
+    },
 };
 </script>
 

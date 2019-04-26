3eb223de6bda7539c5a990cf0db53af071f78230
贺小雷 <xiaolei.he@iluvatar.ai>
2019-04-26 19:29:34 +0800

    feat(button): 支持下拉按钮
    支持下拉按钮

--- packages/ui/components/button/src/Button.vue
+++ packages/ui/components/button/src/Button.vue
@@ -11,20 +11,49 @@
                  block ? 'is-block': ''
         ]"
         @click="handleClick"
+        @mouseover="handleMouseOver"
+        @mouseout="handleMouseOut"
     >
         <i
             :class="['sdxu-button__icon iconfont', iconShown]"
             v-if="iconShown"
         />
         <slot />
+        <span
+            class="sdxu-button__icon-dropdown"
+            v-if="$slots.dropdown"
+            :class="[dropdownVisible ? 'is-reverse' : '']"
+        >
+            <i
+                class="iconfont iconicon-caret-bottom"
+            />
+        </span>
+        <el-collapse-transition>
+            <div
+                class="sdxu-button__dropdown"
+                :style="{width: dropdownWidth}"
+                v-if="$slots.dropdown"
+                v-show="dropdownVisible"
+            >
+                <div class="sdxu-button__dropdown--main">
+                    <slot name="dropdown" />
+                </div>
+            </div>
+        </el-collapse-transition>
     </div>
 </template>
 
 <script>
+import CollapseTransition from 'element-ui/lib/transitions/collapse-transition';
 export default {
     name: 'SdxuButton',
     data() {
-        return {};
+        return {
+            dropdownVisible: false
+        };
+    },
+    components: {
+        [CollapseTransition.name]: CollapseTransition
     },
     props: {
         size: {
@@ -33,7 +62,7 @@ export default {
         },
         type: {
             type: String,
-            default: 'primary' // primary, success, warning, error, default
+            default: 'primary' // primary, success, warning, error, default, text
         },
         invert: {
             type: Boolean,
@@ -62,6 +91,14 @@ export default {
         block: {
             type: Boolean,
             default: false
+        },
+        trigger: {
+            type: String,
+            default: 'hover'
+        },
+        dropdownWidth: {
+            type: String,
+            default: '100px'
         }
     },
     computed: {
@@ -75,8 +112,37 @@ export default {
     },
     methods: {
         handleClick() {
-            this.$emit('click');
+            if (this.trigger === 'click') {
+                this.dropdownVisible = true;
+            } else {
+                this.$emit('click');
+            }
+        },
+        handleMouseOver() {
+            if (this.trigger === 'hover') {
+                this.dropdownVisible = true;
+            } else {
+                this.$emit('mosueover');
+            }
+        },
+        handleMouseOut() {
+            if (this.trigger === 'hover') {
+                this.dropdownVisible = false;
+            } else {
+                this.$emit('mosueout');
+            }
+        },
+        hideDropdown(e) {
+            if (!this.$el.contains(e.target)) {
+                this.dropdownVisible = false;
+            }
         }
+    },
+    mounted() {
+        document.addEventListener('click', this.hideDropdown);
+    },
+    beforeDestroy() {
+        document.removeEventListener('click', this.hideDropdown);
     }
 };
 </script>

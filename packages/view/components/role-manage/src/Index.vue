1248ae33d72bd1eb2014e5b11ff4ddb055236276
xiafei.wu <xiaofei.wu@iluvatar.ai>
2019-05-08 16:34:17 +0800

    feat(role): role

--- packages/view/components/role-manage/src/Index.vue
+++ packages/view/components/role-manage/src/Index.vue
@@ -28,20 +28,24 @@
                 :data="tableData"  
             >
                 <el-table-column
-                    prop="date"
-                    label="日期"
-                />
-                <el-table-column
                     prop="name"
-                    label="姓名"
+                    label="角色名"
                 />
                 <el-table-column
-                    prop="address"
-                    label="地址"
+                    prop="description"
+                    label="角色说明"
+                />
+                <el-table-column
+                    prop="domain"
+                    label="系统类别"
+                />
+                <el-table-column
+                    prop="createdAt"
+                    label="创建时间"
                 />
                 <el-table-column type="expand">
                     <template slot-scope="props">
-                        <span>{{ props.row.desc }}</span>
+                        <span>{{ props.row.description }}</span>
                     </template>
                 </el-table-column>
             </SdxuTable>
@@ -71,6 +75,7 @@ import SdxuButton from '@sdx/ui/components/button';
 import SdxuTable from '@sdx/ui/components/table';
 import SdxuPagination from '@sdx/ui/components/pagination';
 import SdxuDialog from '@sdx/ui/components/dialog';
+import {getRolesList} from '@sdx/utils/src/api/rolemange';
 export default {
     name: 'SdxvRoleManage',
     components: {
@@ -83,38 +88,23 @@ export default {
     data() {
         return {
             value: '',
-            tableData: [{
-                date: '2016-05-02',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1518 弄',
-                key: '11',      
-                desc: ' 那 v 啊女啊 v 那 v 啊美女 v 女你说的 v 你是女 in 在 v 你是 v 好似 u 凤凰文化'
-            }, {
-                date: '2016-05-04',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1517 弄',
-                key: '12',
-                desc: ' 中均居本佛为国内外 i 我无关无关无关无关无关发刚拿上 v 女你说的 v 你是女 in 在 v 你是 v 好似 u 凤凰文化'
-            }, {
-                date: '2016-05-01',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1519 弄',
-                key: '14',
-                desc: ' 关无关无高考文科贡嘎山的噶多少个民生凋敝民生凋敝没舍得不舍得把 v 说关无关无关发刚拿上 v 女你说的 v 你是女 in 在 v 你是 v 好似 u 凤凰文化'
-            }, {
-                date: '2016-05-03',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1516 弄',
-                key: '15',
-                desc: ' 中均居本佛为国内外 iv 你是女 in 在 v 你是 v 好似 u 凤凰文化'
-            }],
+            tableData: [],
             current: 1,
             pageSize: 10,
-            total: 123,
+            total: 0,
             dialogVisible: false
         };
     },
     props: {
+     
+    },
+    created() {
+        getRolesList()
+            .then(data =>{
+                this.tableData = data.roles;
+                this.total = data.total;
+            });
+           
        
     },
     methods: {

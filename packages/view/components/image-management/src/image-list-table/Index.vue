aa577dbdefe13346963428b619b7cbf0ac24e849
yaying.li <yaying.li@sky-data.cn>
2019-05-14 10:28:14 +0800

    feat(view): 初始化

---
+++ packages/view/components/image-management/src/image-list-table/Index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="">
+        image list table
+    </div>
+</template>
+
+<script>
+export default {
+    name: '',
+    data() {
+        return {};
+    },
+    components: {}
+};
+</script>
+
+<style scoped lang="scss">
+</style>

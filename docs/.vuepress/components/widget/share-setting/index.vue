1f42b5ba29d23c7ea1604b9ae4f2bdba89c48c1f
yaying.li <yaying.li@sky-data.cn>
2019-05-30 11:00:08 +0800

    feat(widget): 共享设置组件

---
+++ docs/.vuepress/components/widget/share-setting/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="index">
+        选择共享设置
+        <img :src="imgUrl" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'index',
+    data() {
+        return {
+            imgUrl:require("../../../public/widget/share-setting/index.png")
+        };
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .index {
+        width: 400px;
+    }
+</style>

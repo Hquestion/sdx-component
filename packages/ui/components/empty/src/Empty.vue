893cc3a1f288f4c0aded08c6d6c9b844bd2b8f25
xiafei.wu <xiaofei.wu@iluvatar.ai>
2019-06-22 12:52:24 +0800

    feat: empty 组件

--- packages/ui/components/empty/src/Empty.vue
+++ packages/ui/components/empty/src/Empty.vue
@@ -1,18 +1,32 @@
 <template>
     <div class="sdxu-empty">
         <div>
-            <i class="sdx-icon sdx-icon-warning sdxu-empty__icon"></i>
+            <i
+                class="sdx-icon sdx-icon-warning sdxu-empty__icon"
+                v-if="emptyType === 'warning'"
+            />
+            <svg
+                class="icon"
+                aria-hidden="true"
+            >
+                <use xlink:href="#iconwushuju" />
+            </svg>
         </div>
         <div class="sdxu-empty__desc">
             <slot>暂无内容</slot>
         </div>
-
     </div>
 </template>
 
 <script>
 export default {
     name: 'SdxuEmpty',
+    props: {
+        emptyType: {
+            type: String,
+            default: 'nodata',
+        }
+    },
     data() {
         return {};
     }

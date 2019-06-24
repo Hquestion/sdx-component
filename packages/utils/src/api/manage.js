fb1fe152adabe9d2d3beae31ff0c741adb4a6834
xiafei.wu <xiaofei.wu@iluvatar.ai>
2019-06-24 11:07:44 +0800

    feat: 镜像列表状态更改

--- packages/utils/src/api/manage.js
+++ packages/utils/src/api/manage.js
@@ -1,5 +1,4 @@
-// import httpService from '../http-service';
-import httpService from '@sdx/utils/lib/http-service';
+import httpService from '../http-service';
 import { COMPOSE_GATEWAY_BASE } from './config';
 
 const userProfilesApi = `${COMPOSE_GATEWAY_BASE}user-profiles/`;

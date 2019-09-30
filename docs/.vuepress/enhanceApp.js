98df6eced50f08bddd35412d0ae98775c923456a
xiaolei.he <xiaolei.he@iluvatar.com>
2019-09-30 11:08:31 +0800

    fix(文档): 引入iconfont导致报错的bug修复
    引入iconfont导致报错的bug修复

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -12,12 +12,17 @@ import '@sdx/view/theme/index.scss';
 import '../.vuepress/public/css/index.css'
 
 export default ({
-  Vue, // VuePress 正在使用的 Vue 构造函数
-  options, // 附加到根实例的一些选项
-  router, // 当前应用的路由实例
-  siteData // 站点元数据
-}) => {
-  // ...做一些其他的应用级别的优化
-  Vue.use(VueHighlightJS)
-  Vue.use(Element)
+                    Vue, // VuePress 正在使用的 Vue 构造函数
+                    options, // 附加到根实例的一些选项
+                    router, // 当前应用的路由实例
+                    siteData // 站点元数据
+                }) => {
+    // ...做一些其他的应用级别的优化
+    Vue.use(VueHighlightJS)
+    Vue.use(Element)
+    Vue.mixin({
+        mounted() {
+            import('../../packages/utils/src/theme-common/iconfont/iconfont.js');
+        }
+    })
 }
---
+++ docs/.vuepress/replace.js
@@ -0,0 +1,2 @@
+// iconfont.js在mounted之前引入报错，这里通过alias将iconfont.js替换为本文件，再通过全局混入引入iconfont.js
+export default {};

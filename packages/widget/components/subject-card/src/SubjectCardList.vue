cbd0c2523f4a042ca4fdfcf4c65b3a3b04766a49
xiafei.wu <xiaofei.wu@iluvatar.ai>
2019-10-14 16:26:17 +0800

    feat: subjectcard

---
+++ packages/widget/components/subject-card/src/SubjectCardList.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="sdxw-subject-card-list">
+        <slot /> 
+    </div>    
+</template>
+
+<script>
+export default {
+    name: 'SdxwSubjectCardList'
+};
+</script>
+
+<style>
+
+</style>

bcad48fb7c0898f64931ae9b61f737f625085f2e
yaying.li <yaying.li@iluvatar.ai>
2019-10-29 11:28:27 +0800

    feat(view): task running limit

--- packages/widget/components/task-running-limit/src/TaskRunningLimit.vue
+++ packages/widget/components/task-running-limit/src/TaskRunningLimit.vue
@@ -1,12 +1,25 @@
 <template>
     <el-alert
         v-if="maxCpuTime && maxGpuTime"
-        :title="t('widget.taskRunningLimit.info1') + maxGpuTime + t('widget.taskRunningLimit.info2') + maxCpuTime + t('widget.taskRunningLimit.info3')"
         type="info"
-        show-icon
-        closable
-        style="height: 50px"
-    />
+        :closable="false"
+        style="height: 50px;background-color: #DEEBFF;color: #13264D;"
+    >
+        <div slot="title">
+            <svg
+                style="width: 1em; height: 1em;
+                        vertical-align: -0.15em;
+                        fill: currentColor;
+                        overflow: hidden;
+                        font-size: 14px;
+                        margin-right: 4px;"
+                aria-hidden="true"
+            >
+                <use xlink:href="#sdx-tiaofutishi" />
+            </svg>
+            {{ t('widget.taskRunningLimit.info1') + maxGpuTime + t('widget.taskRunningLimit.info2') + maxCpuTime + t('widget.taskRunningLimit.info3') }}
+        </div>
+    </el-alert>
 </template>
 
 <script>

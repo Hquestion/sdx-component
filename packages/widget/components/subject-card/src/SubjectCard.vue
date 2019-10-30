2cfd3e89a2373d284b37573f9ae96e6bb8b0cd12
xiafei.wu <xiaofei.wu@iluvatar.ai>
2019-10-30 15:02:28 +0800

    feat: 项目任务卡片

--- packages/widget/components/subject-card/src/SubjectCard.vue
+++ packages/widget/components/subject-card/src/SubjectCard.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="sdxw-subject-card"
+        :class="['sdxw-subject-card', `sdxw-subject-card--${meta.type}`]"
     >
         <div class="sdxw-subject-card__header">
             <div class="sdxw-subject-card__icon">
@@ -13,7 +13,10 @@
                     {{ meta.iconName }}
                 </div>
             </div>
-            <div class="sdxw-subject-card__content">
+            <div
+                class="sdxw-subject-card__content"
+                v-if="meta.type === 'project'"
+            >
                 <div class="sdxw-subject-card__content--info">
                     <div class="left">
                         <span class="title">
@@ -21,9 +24,38 @@
                         </span>
                         <span
                             class="task"
-                            v-if="meta.type === 'project'"
                         >
                             {{ `${meta.taskNumber}${meta.taskNumber === 0 ? t('view.project.taskCount') : t('view.project.taskCounts')}` }}
+                        </span>
+                    </div>
+                    <div class="right">
+                        <div class="creator">
+                            {{ (meta && meta.creator ) || '' }}
+                            <span v-if="getUser().userId === meta.owner.uuid">({{ t('view.project.oneself') }})</span>
+                        </div>
+                        <i class="customize-icon" />
+                        <div>
+                            {{ `${t('view.project.Created')}${dateFormatter(meta && meta.createdAt)}` }}
+                        </div>
+                    </div>
+                </div>
+                <div
+                    class="sdxw-subject-card__content--description"
+                >
+                    {{ meta && meta.description }}
+                </div>
+            </div>
+            <div
+                v-if="meta.type === 'task'"
+                class="sdxw-subject-card__content"
+            >
+                <div class="sdxw-subject-card__content--top">
+                    <div class="left">
+                        <span
+                            class="title"
+                            @click="$emit('operate', {id: meta.uuid, type: 'detail'})"
+                        >
+                            {{ meta && meta.title }}
                         </span>
                     </div>
                     <div class="right">
@@ -35,26 +67,25 @@
                         >
                             {{ meta.state && meta.state.statusText || 'null-state' }}
                         </SdxwFoldLabel>
-                        <i
-                            v-if="meta.state && meta.state.type"
-                            class="customize-icon"
-                        />
-                        <div class="creator">
-                            {{ (meta && meta.creator ) || '' }}
-                            <span v-if="getUser().userId === meta.owner.uuid">({{ t('view.project.oneself') }})</span>
-                        </div>
-                        <i class="customize-icon" />
-                        <div>
-                            {{ `${t('view.project.Created')}${dateFormatter(meta && meta.createdAt)}` }}
-                        </div>
                     </div>
                 </div>
-                <div class="sdxw-subject-card__content--description">
-                    {{ meta && meta.description }}
+                <div class="sdxw-subject-card__content--bottom">
+                    <div class="creator">
+                        {{ (meta && meta.creator ) || '' }}
+                        <span v-if="getUser().userId === meta.owner.uuid">({{ t('view.project.oneself') }})</span>
+                    </div>
+                    <div>
+                        {{ `${t('view.project.Created')}${dateFormatter(meta && meta.createdAt)}` }}
+                    </div>
                 </div>
             </div>
         </div>
-
+        <div
+            class="sdxw-subject-card__taskdesc"
+            v-if="meta.type === 'task'"
+        >
+            {{ meta && meta.description }}
+        </div>
         <div
             class="sdxw-subject-card__footer"
             v-if="meta.footer !== false"
@@ -112,7 +143,7 @@ export default {
         meta: {
             type: Object,
             default: () => {}
-        }
+        },
     },
     data() {
         return {

c888b39b0745d74c745a8bc571ba9287e99ce0d7
xiafei.wu <xiaofei.wu@iluvatar.ai>
2019-11-01 14:34:39 +0800

    feat: 任务管理

---
+++ packages/utils/src/mixins/task2.js
@@ -0,0 +1,13 @@
+import { t } from '../locale';
+
+export default {
+    data() {
+        return {
+            // note: 将请求列表的函数赋给该变量，用于执行操作后刷新列表用
+            fetchDataMinxin: null
+        };
+    },
+    methods: {
+        
+    },
+};

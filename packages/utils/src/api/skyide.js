4f3d4976de6f1bb9fabab87dfdaf4b03f420e417
hexiaolei <1035271660@qq.com>
2019-11-14 20:04:38 +0800

    chore(view): 修改url

--- packages/utils/src/api/skyide.js
+++ packages/utils/src/api/skyide.js
@@ -2,18 +2,18 @@ import httpService from '../http-service';
 import { IDE_MANAGER_GATEWAY_BASE } from './config';
 
 export function configIDE(taskId, config = {}) {
-    return httpService.post(`${IDE_MANAGER_GATEWAY_BASE}/ides`, {
+    return httpService.post(`${IDE_MANAGER_GATEWAY_BASE}ides`, {
         ...config,
         taskId
     });
 }
 
 export function createSession(ideUuid, params) {
-    return httpService.post(`${IDE_MANAGER_GATEWAY_BASE}/ides/${ideUuid}/notebook_sessions`, params);
+    return httpService.post(`${IDE_MANAGER_GATEWAY_BASE}ides/${ideUuid}/notebook_sessions`, params);
 }
 
 export function shutdownSession(ideUuid, sessionId) {
-    return httpService.remove(`${IDE_MANAGER_GATEWAY_BASE}/ides/${ideUuid}/notebook_sessions/${sessionId}`);
+    return httpService.remove(`${IDE_MANAGER_GATEWAY_BASE}ides/${ideUuid}/notebook_sessions/${sessionId}`);
 }
 
 export function createTerminal(ideUuid) {
